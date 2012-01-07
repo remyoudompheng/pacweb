a11175d370d148569e7dffa9782d679f90eaeed1
Rémy Oudompheng <remy@archlinux.org>
2012-01-07 17:35:21 +0100

    Add CPU profiling option.

--- pacweb.go
+++ pacweb.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"runtime/pprof"
 )
 
 var logger = log.New(os.Stderr, "pacweb ", log.LstdFlags|log.Lshortfile)
@@ -13,9 +15,40 @@ func logRequest(req *http.Request) {
 	logger.Printf("%s %s from %s", req.Method, req.URL, req.RemoteAddr)
 }
 
+var (
+	profile = flag.String("pprof", "", "Dump CPU profile at that path")
+)
+
+func sigHandle() {
+	for sig := range signal.Incoming {
+		logger.Printf("caught signal %v", sig)
+		switch sig {
+		case os.SIGTERM, os.SIGINT:
+			if *profile != "" {
+				pprof.StopCPUProfile()
+			}
+			logger.Printf("exiting.")
+			os.Exit(1)
+		}
+	}
+}
+
 func main() {
 	listen := flag.String("http", "localhost:8070", "Address to listen on")
 	flag.Parse()
+
+	if *profile != "" {
+		f, er := os.Create(*profile)
+		if er != nil {
+			panic(er)
+		}
+		er = pprof.StartCPUProfile(f)
+		if er != nil {
+			panic(er)
+		}
+	}
+
+	go sigHandle()
 	logger.Printf("starting HTTP server at %s", *listen)
 	http.ListenAndServe(*listen, nil)
 }
